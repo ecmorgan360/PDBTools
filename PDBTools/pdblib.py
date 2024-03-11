2571cc42a2c6c71a93e9bc8c718af4f51e9a44db
ecmorgan360 <g20m8656@campus.ru.ac.za>
2024-03-11 11:08:05 +0200

    Added PDBTools package, pdblib module and checkPDB.py script

---
+++ PDBTools/pdblib.py
@@ -0,0 +1,4 @@
+import requests
+
+def check_imported():
+    print("The pdblib module has been imported and is being accessed.")
---
+++ checkPDB.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+from PDBTools import pdblib
+
+pdblib.check_imported()
